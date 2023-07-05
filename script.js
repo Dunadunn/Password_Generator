f4a492f34d5df69195a9ee6e5b2793fc051023e7
Dunadunn <alxrodrig7@gmail.com>
2023-07-04 21:54:01 -0700

    Updated javascript code from previous attempt that was initially used for project #2. Expected behavior of script.js is to add prompts for the user's input, confirm what type of characters the user would like to generate a unique and secure password.

--- script.js
+++ script.js
@@ -3,23 +3,46 @@ document.getElementById('generate').addEventListener('click', function() {
     var upperCaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     var numbers = '0123456789';
     var specialCharacters = '!@#$%^&*()';
-    var length = 10;
+    var characters = '';
     var password = '';
 
-    // Expected to ensure one character of each type in the password
-    password += lowerCaseLetters[Math.floor(Math.random() * lowerCaseLetters.length)];
-    password += upperCaseLetters[Math.floor(Math.random() * upperCaseLetters.length)];
-    password += numbers[Math.floor(Math.random() * numbers.length)];
-    password += specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
-
-    // Fill up the remaining characters in the password
-    for (var i = password.length; i < length; i++) {
-        var characters = lowerCaseLetters + upperCaseLetters + numbers + specialCharacters;
-        password += characters[Math.floor(Math.random() * characters.length)];
+    var length = parseInt(prompt("Choose a length for your password between 8 and 24 characters"));
+    while (isNaN(length) || length < 8 || length > 24) {
+        length = parseInt(prompt("Invalid input. Please enter a number between 8 and 24."));
     }
 
-    // Shuffle password to ensure random position of character types
-    password = password.split('').sort(function(){return 0.5-Math.random()}).join('');
+    if (confirm("Would you like to include lowercase characters?")) {
+        characters += lowerCaseLetters;
+    }
+    if (confirm("Would you like to include uppercase characters?")) {
+        characters += upperCaseLetters;
+    }
+    if (confirm("Would you like to include numbers?")) {
+        characters += numbers;
+    }
+    if (confirm("Would you like to include special characters?")) {
+        characters += specialCharacters;
+    }
+
+    while (characters.length === 0) {
+        alert("You must select at least one character type.");
+        if (confirm("Would you like to include lowercase characters?")) {
+            characters += lowerCaseLetters;
+        }
+        if (confirm("Would you like to include uppercase characters?")) {
+            characters += upperCaseLetters;
+        }
+        if (confirm("Would you like to include numbers?")) {
+            characters += numbers;
+        }
+        if (confirm("Would you like to include special characters?")) {
+            characters += specialCharacters;
+        }
+    }
+
+    for (var i = 0; i < length; i++) {
+        password += characters[Math.floor(Math.random() * characters.length)];
+    }
 
     document.getElementById('password').innerText = password;
 });
